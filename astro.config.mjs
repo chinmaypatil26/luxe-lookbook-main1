60d0b615d7cbfb587380dd99ce4737b705c04572
chinmaypatil26 <almostwon89@gmail.com>
2025-11-26 13:55:16 +0530

    sixth commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import cloudProviderFetchAdapter from "@wix/cloud-provider-fetch-adapter";
 import wix from "@wix/astro";
 import monitoring from "@wix/monitoring-astro";
 import react from "@astrojs/react";
 import sourceAttrsPlugin from "@wix/babel-plugin-jsx-source-attrs";
 import dynamicDataPlugin from "@wix/babel-plugin-jsx-dynamic-data";
 import customErrorOverlayPlugin from "./vite-error-overlay-plugin.js";
+import vercel from "@astrojs/vercel/serverless";
+import cloudProviderFetchAdapter from "@wix/cloud-provider-fetch-adapter";
 
 const isBuild = process.env.NODE_ENV == "production";
+const isVercel = process.env.VERCEL === "1";
 
 // https://astro.build/config
 export default defineConfig({
@@ -50,7 +52,11 @@ export default defineConfig({
       customErrorOverlayPlugin(),
     ],
   },
-  adapter: isBuild ? cloudProviderFetchAdapter({}) : undefined,
+  adapter: isBuild
+    ? isVercel
+      ? vercel()
+      : cloudProviderFetchAdapter({})
+    : undefined,
   devToolbar: {
     enabled: false,
   },
